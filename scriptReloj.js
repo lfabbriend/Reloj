9f23a4e197fcf8a69b908c6706810116804d3049
lfabbriend <lucia.fabbri@endava.com>
2022-01-27 13:34:07 -0300

    Genial

--- scriptReloj.js
+++ scriptReloj.js
@@ -1,42 +1,50 @@
-function setTime() {
-    horas = new Date().getHours();
-    minutos = new Date().getMinutes();
-    segundos = new Date().getSeconds();
-    return horas, minutos, segundos;
+function addZero(number) {
+    return ("0" + number).slice(-2);
+}
+
+function getTime() {
+    const tiempo = new Date();
+    const horas = tiempo.getHours();
+    const minutos = tiempo.getMinutes();
+    const segundos = tiempo.getSeconds();
+    return {horas, minutos, segundos};
 }
 
 function relojAnalogico() {
-   setTime();
 
-    if (horas>=12) {
+   const {horas,minutos,segundos} = getTime();
+
+   /* if (hgetTs>=12) {
         horas = horas ;
-        porcentajeHoras = horas / 24 * 360;  /*fijarse si funciona después de las 12*/
+        porcentajeHoras = horas / 24 * 360;  fijarse si funciona después de las 12
     }
 
     else {
         porcentajeHoras = horas / 12 * 360;
     }
+*/
 
-    porcentajeHoras += minutos / 60 * 30;
-    porcentajeMinutos = minutos / 60 *360;
-    porcentajeSegundos = segundos / 60 * 360;
 
-    document.getElementById("horas").style.transform = "rotate("+porcentajeHoras+"deg)";
-    document.getElementById("minutos").style.transform = "rotate("+porcentajeMinutos+"deg)";
-    document.getElementById("segundos").style.transform = "rotate("+porcentajeSegundos+"deg)";
-}
+    porcentajeHoras = 360 / 12 * horas;
+    porcentajeMinutos = 360 / 60 * minutos;
+    porcentajeSegundos = 360 / 60 * segundos;
+
+    document.getElementById("horas").style.setProperty("--hours-rotation", porcentajeHoras+"deg");
+    document.getElementById("minutos").style.setProperty("--mins-rotation", porcentajeMinutos+"deg");
+    document.getElementById("segundos").style.setProperty("--segs-rotation", porcentajeSegundos+"deg");
+
+
+    /*document.getElementById("horas").style.transform = "translate(-50%,-50%) rotate("+porcentajeHoras+"deg)";
+    document.getElementById("minutos").style.transform = "translate(-50%,-50%) rotate("+porcentajeMinutos+"deg)";
+    document.getElementById("segundos").style.transform = "translate(-50%,-50%) rotate("+porcentajeSegundos+"deg)";
+*/}
 
 setInterval(relojAnalogico,1000);
 
 let html = document.getElementById("tiempo");
 
 setInterval(function() {
-    setTime();
-    if (horas<10)
-        horas = '0'+horas;
-    if (minutos<10)
-     minutos = '0'+minutos; 
-     if (segundos<10)
-        segundos = '0'+horas;   
-    html.innerHTML = horas+" : "+minutos+" : "+segundos;
+    const {horas,minutos,segundos} = getTime();
+
+    html.innerHTML = addZero(horas)+" : "+addZero(minutos)+" : "+addZero(segundos);
 },1000);
